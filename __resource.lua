9fe3b1221e2f6b7a357869a10a0009cd4d7f4aea
MenanAk47 <35970476+MenanAk47@users.noreply.github.com>
2021-11-18 11:47:19 +0600

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
+name "ak47_drugdealer"
+author "MenanAk47 (MenanAk47#3129)"
 description 'AK47 DRUG DEALER'
-
 version '1.0'
 
 client_scripts {
